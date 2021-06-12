84486459889f35a12489744474b137ec075d3172
MinhNhat308 <80838485+MinhNhat308@users.noreply.github.com>
2021-06-12 15:33:36 +0700

    Add files via upload

---
+++ BMW/BMW/includes/header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>memorial books</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div class="logo"> <a href="index.html"><img src="images/logo.png" alt="#"></a> </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <div class="menu-area">
+                        <div class="limit-box">
+                           <nav class="main-menu">
+                              <ul class="menu-area-main">
+                                 <li > <a href="index.html">Home</a> </li>
+                                 <li class="active"> <a href="about.html">About us</a> </li>
+                                 <li><a href="books.html">Our Books</a></li>
+                                 <li><a href="library.html">library</a></li>
+                                 <li><a href="contact.html">Contact us</a></li>
+                                 <li class="mean-last"> <a href="#"><img src="images/search_icon.png" alt="#" /></a> </li>
+                                 <li class="mean-last"> <a href="#"><img src="images/top-icon.png" alt="#" /></a> </li>
+                              </ul>
+                           </nav>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+         <!-- end header inner -->
+      </header>
+      <!-- end header -->
---
+++ BMW/BMW/index.php
@@ -0,0 +1,227 @@
+<?php
+include 'includes/header.php';
+ ?>      <!-- end header -->
+      <section class="slider_section">
+         <div id="myCarousel" class="carousel slide banner-main" data-ride="carousel">
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <img class="first-slide" src="images/BMWWP.jpg" alt="First slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1>The iX<br>New Generation Electric</h1>
+
+                        <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/future-vehicles/ix.html">Learn More</a>  </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <img class="second-slide" src="images/BMW3.jpg" alt="Second slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1>The M5 CS<br> DISCOVER THE FASTEST<br>STREET-LEGAL BMW EVER</h1>
+                        <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/future-vehicles/m5-cs-sedan.html">Learn More</a>  </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <img class="third-slide" src="images/BMW4.jpg" alt="Third slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1>SHOP FROM ANYWHERE<br> </h1>
+                        <p>With BMW's online shopping tools, buying your dream car is as easy as browsing </p>
+                        <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/buy-online.html">Learn More</a>  </div>
+                        <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/inventory.html#!/results/">Shop Models</a>  </div>
+
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+            </a>
+         </div>
+      </section>
+      <div class="front-2">
+        <div class="column-2">
+          <img src="images/BMWwall1.jpg" alt="">
+          <div class="text-block10">
+          <h10> The 2021 BMW X7</h10>
+          <p10> EXPERIENCE A LEGEND </p10>
+          <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/vehicles/x-models/x7/sports-activity-vehicle/overview.html">Learn More</a>  </div>
+
+          </div>
+        </div>
+        <div class="column-2">
+          <img src="images/BMWwall2.png" alt="">
+          <div class="text-block10">
+          <h10> The 2021 BMW X5</h10>
+          <p10> LEAD WITH CONFIDENCE </p10>
+          <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/vehicles/x-models/x5/sports-activity-vehicle/overview.html">Learn More</a>  </div>
+        </div>
+        </div>
+      </div>
+      <div class="front-3">
+        <div class="column-3">
+          <img src="images/BMWwall5.jpg" alt="">
+          <div class="text-block10">
+          <h10> The 2021 BMW i7</h10>
+          <p10> THE FASTEST CAR OF THE WORLD </p10>
+          <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/vehicles/5-series/sedan/overview.html">Learn More</a>  </div>
+
+          </div>
+        </div>
+        <div class="column-3">
+          <img src="images/BMWwall4.jpg" alt="">
+          <div class="text-block10">
+          <h10> The 2021 BMW i7</h10>
+          <p10> LEAD WITH CONFIDENCE </p10>
+          <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/vehicles/8-series/overview.html">Learn More</a>  </div>
+        </div>
+        </div>
+      </div>
+
+
+      <!-- Library -->
+      <div class="Library">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-10 offset-md-1">
+                  <div class="titlepage">
+                     <h2>BUILD <strong class="black">the BMW Of Your Dreams </strong></h2>
+
+                  </div>
+               </div>
+            </div>
+         </div>
+       </div>
+
+  <div class="front-4">
+      <div class="column-4">
+        <img src="images/list1.jpg" alt="">
+        <div class="exp1">
+          <h10>3 SERIES SEDAN</h10>
+          <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/build-your-own.html#/series/3/sedan">START DESIGNING</a>  </div>
+        </div>
+      </div>
+      <div class="column-4">
+        <img src="images/list2.jpg" alt="">
+        <div class="exp1">
+          <h10>X3</h10>
+          <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/build-your-own.html#/series/X3/sports-activity-vehicle">START DESIGNING</a>  </div>
+      </div>
+      </div>
+      <div class="column-4">
+        <img src="images/list3.jpg" alt="">
+        <div class="exp1">
+          <h10>5 SERIES BMW</h10>
+        <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/build-your-own.html#/series/5/sedan">START DESIGNING</a>  </div>
+        </div>
+      </div>
+      <div class="column-4">
+        <img src="images/list4.jpg" alt="">
+        <div class="exp1">
+          <h10>X5</h10>
+          <div class="button_section"> <a class="main_bt" href="https://www.bmwusa.com/build-your-own.html#/series/X5/sports-activity-vehicle">START DESIGNING</a>  </div>
+        </div>
+      </div>
+</div>
+<section class="slider_section">
+
+   <div id="myCarousel" class="carousel slide banner-main" data-ride="carousel">
+      <div class="carousel-inner">
+         <div class="carousel-item active">
+            <img class="first-slide" src="images/WP1.jpg" alt="First slide">
+            <div class="container">
+               <div class="carousel-caption relative">
+                  <h1>REWARDS FOR  <br>THE DRIVEN</h1>
+                  <p> Accelarate your rewards with the new BMW credit cards. </p>
+                  <div class="button_section"> <a class="main_bt" href="https://www.mybmwcreditcard.com/offer1">Learn More</a>  </div>
+               </div>
+            </div>
+         </div>
+         <div class="carousel-item">
+            <img class="second-slide" src="images/WP2.jpg" alt="Second slide">
+            <div class="container">
+               <div class="carousel-caption relative">
+                  <h1>BMW VALUE SERVICE</h1>
+                  <p> Think we're expensive? Think again. </p>
+
+                  <div class="button_section"> <a class="main_bt" href="https://bmwusaservice.com/valueservice?utm_source=FMA&utm_campaign=JanFMAValue">Learn More</a>  </div>
+               </div>
+            </div>
+         </div>
+         <div class="carousel-item">
+            <img class="third-slide" src="images/WP3.jpg" alt="Third slide">
+            <div class="container">
+               <div class="carousel-caption relative">
+                  <h1>Protective MEASURES<br> </h1>
+                  <p>Keep your BMW pristine with brand-new floor mats. </p>
+                  <div class="button_section"> <a class="main_bt" href="https://www.shopbmwusa.com/ACCESSORIES/INTERIOR/FLOOR-MATS?utum_source=BMWUSA_FMA&utum_medium=National_FMA&utum_campaing=FloormatsFMA">Learn More</a>  </div>
+                  <div class="button_section"> <a class="main_bt" href="https://www.shopbmwusa.com/ACCESSORIES/INTERIOR/FLOOR-MATS?utum_source=BMWUSA_FMA&utum_medium=National_FMA&utum_campaing=FloormatsFMA">Shop Now</a>  </div>
+
+               </div>
+            </div>
+         </div>
+
+      </div>
+      <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+      </a>
+   </div>
+   <div class="Library">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-10 offset-md-1">
+               <div class="titlepage">
+                  <h2>OWNERSHIPS <strong class="black">Start Here </strong></h2>
+
+               </div>
+            </div>
+         </div>
+      </div>
+    </div>
+    <div class="front-5">
+        <div class="column-5">
+          <div class="exp2">
+            <h1>Trade-In Value</h1>
+          <p> Get the latest information on your vehicle’s trade-in value today.</p>
+            <div class="button_section"> <a class="main_bt1" href="https://www.intelliprice.com/intellipricedealer/start.htm?dealerid=952001">VISIT BLACK BOOK</a>  </div>
+          </div>
+        </div>
+        <div class="column-5">
+          <div class="exp2">
+            <h1>Check Your Credit Score</h1>
+          <p>See where you stand on your journey to owning a BMW..</p>
+            <div class="button_section"> <a class="main_bt1" href="https://www.intelliprice.com/intellipricedealer/start.htm?dealerID=9541001">VISI EQUIFAX</a>  </div>
+        </div>
+        </div>
+        <div class="column-5">
+
+          <div class="exp2">
+            <h1>Apply for Financing</h1>
+          <p>Own the BMW of your dreams with BMW Financial Services.</p>
+          <div class="button_section"> <a class="main_bt1" href="https://creditapp.bmwusa.com/">GET STARTED</a>  </div>
+          </div>
+        </div>
+        <div class="column-5">
+
+          <div class="exp2">
+            <h1>Stay Connected</h1>
+          <p>Receive the latest offers, releases, and news from BMW./p>
+            <div class="button_section"> <a class="main_bt1" href="https://www.bmwusa.com/keep-me-updated.html">SIGN UP NOW</a>  </div>
+          </div>
+        </div>
+  </div>
+  <?php
+  include 'includes/footer.php';
+   ?>
